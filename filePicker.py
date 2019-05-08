2f6da922736bd46f305b1d861ccc4ab0735d84cc
Roober Cruz <41974124+Tezaen@users.noreply.github.com>
2019-05-08 15:33:10 -0700

    Add files via upload
    Here is a file picker. However, I haven't figured out how to display or save an image data as a variable for the OpenCV/translator part yet. I will upload a second version when I figure it out.

---
+++ filePicker.py
@@ -0,0 +1,37 @@
+#FIle Picker tester
+#https://www.tutorialspoint.com/pyqt/pyqt_qfiledialog_widget.htm
+import sys
+from PyQt5 import *
+from PyQt5.QtCore import QObject, pyqtSlot
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QFileDialog, QVBoxLayout, QPushButton
+from PyQt5.QtGui import QPixmap
+
+class filePicker(QWidget):
+	def __init__(self):
+		super().__init__()
+		vbox = QVBoxLayout()
+		self.button = QPushButton("Upload Image", self)
+		self.labels = QLabel(self)
+		self.button.clicked.connect(self.on_click)
+		vbox.addWidget(self.button)
+		vbox.addWidget(self.labels)
+
+		self.setLayout(vbox)
+	@pyqtSlot()
+	def on_click(self):
+		fname = QFileDialog.getOpenFileName(self, 'Open File', '/home', "Image Files (*.jpg *.png)")
+		self.labels.setPixmap(QPixmap(fname))
+
+
+myApp = QApplication(sys.argv)
+myWindow = filePicker()
+myWindow.setGeometry(100, 100, 1200, 800)
+myWindow.setWindowTitle("Hello")
+myWindow.show()
+
+sys.exit(myApp.exec_())
+
+if __name__ == '__main__':
+   main()
+
+

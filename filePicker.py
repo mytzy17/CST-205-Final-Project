44a8e653ac7425fedbc945d8492ff3c6c51a9c52
Roober Cruz <41974124+Tezaen@users.noreply.github.com>
2019-05-09 20:46:32 -0700

    Fixed the filePicker.py and in this program, it...
    ... will display the image. Use fname as a way to use the image in the translation

---
+++ filePicker.py
@@ -0,0 +1,39 @@
+#FIle Picker tester
+#https://www.tutorialspoint.com/pyqt/pyqt_qfiledialog_widget.htm
+import sys
+from PyQt5 import *
+from PyQt5.QtCore import QObject, pyqtSlot
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QFileDialog, QVBoxLayout, QPushButton
+from PyQt5.QtGui import QPixmap
+
+class filePicker(QWidget):
+	def __init__(self):
+		super().__init__()
+		vbox = QVBoxLayout()
+		self.button = QPushButton("Upload Image", self)
+		self.labels = QLabel(self)
+		self.button.clicked.connect(self.on_click)
+		vbox.addWidget(self.button)
+		vbox.addWidget(self.labels)
+
+		self.setLayout(vbox)
+	@pyqtSlot()
+	def on_click(self):
+		fname, _ = QFileDialog.getOpenFileName(self.labels, 'Open File', '/usr/tmp', "Images (*.jpg *.png)")
+		#imageFile = open(fname)
+		#data = imageFile.read()
+		self.labels.setPixmap(QPixmap(fname))
+
+
+myApp = QApplication(sys.argv)
+myWindow = filePicker()
+myWindow.setGeometry(100, 100, 1200, 800)
+myWindow.setWindowTitle("Hello")
+myWindow.show()
+
+sys.exit(myApp.exec_())
+
+if __name__ == '__main__':
+   main()
+
+

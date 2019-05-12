1caf84eabe502f18b7d235aae33dfc30416e93df
Denize Ignacio <denizeignacio@gmail.com>
2019-05-12 09:55:58 -0700

    Translated speech and text

--- function.py
+++ function.py
@@ -1,15 +1,27 @@
 from PIL import Image
-#from google.cloud import translate
+from google.cloud import translate
 import pyttsx3
 import pytesseract
 
-def trans():
-    engine = pyttsx3.init()
+translate_client = translate.Client()
+result = translate_client.translate(
+    text, target_language=target)
 
-    text = pytesseract.image_to_string(Image.open('images/testImg1.png'))
-    #client = translate.Client()
-   # client.get_languages()
-    text = text.replace('\n', ' ')
-    engine.say(text)
-    engine.runAndWait()
-trans()
+engine = pyttsx3.init()
+text = pytesseract.image_to_string(Image.open(path))
+
+class ImageToText:
+    def speech(text, path):
+        #client = translate.Client()
+       # client.get_languages()
+        text = text.replace('\n', ' ')
+        engine.say(text)
+        engine.runAndWait()
+
+    def translate(text, target):
+        print(u'Text: {}'.format(result['input']))
+        print(u'Translation: {}'.format(result['translatedText']))
+        print(u'Detected source language: {}'.format(result['detectedSourceLanguage']))
+
+
+
